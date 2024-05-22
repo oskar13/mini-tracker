3f3f462dd026d288c3175167203b2b6fc21ee5d3
oskar13 <oskar_ehaver@msn.com>
2024-05-22 06:06:27 +0300

    add login page form

--- pkg/web/handlers/login-page.go
+++ pkg/web/handlers/login-page.go
@@ -1,12 +1,19 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
+
+	db "github.com/oskar13/mini-tracker/pkg/db"
+	webutils "github.com/oskar13/mini-tracker/pkg/web/webUtils"
 )
 
 func LoginPage(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "hello")
+	userData := webutils.GetUserData(r, db.DB)
+
+	webutils.RenderTemplate(w, []string{"pkg/web/templates/login.html",
+		"pkg/web/templates/sidebar.html", "pkg/web/templates/head.html",
+		"pkg/web/templates/end.html",
+		"pkg/web/templates/commandbar.html"}, userData)
 
 }
