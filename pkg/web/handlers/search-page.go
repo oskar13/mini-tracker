a1d6b9e2b5ff9acc83a231a2cc813c66c7b218d4
oskar13 <oskar_ehaver@msn.com>
2025-04-20 00:00:48 +0300

    edit search page imports

--- pkg/web/handlers/search-page.go
+++ pkg/web/handlers/search-page.go
@@ -1,18 +1,17 @@
 package handlers
 
 import (
-	"net/http"
-
+	"github.com/gin-gonic/gin"
 	"github.com/oskar13/mini-tracker/pkg/web/accounts"
 	webutils "github.com/oskar13/mini-tracker/pkg/web/webUtils"
 	"github.com/oskar13/mini-tracker/pkg/web/webdata"
 )
 
-func TorrentSearchPage(w http.ResponseWriter, r *http.Request) {
+func TorrentSearchPage(c *gin.Context) {
+	userData := accounts.GetUserData(c.Request)
 
-	userData := accounts.GetUserData(r)
-
-	if !accounts.CheckLogin(w, r, userData) {
+	// Check login
+	if !accounts.CheckLogin(c.Writer, c.Request, userData) {
 		return
 	}
 
@@ -28,9 +27,12 @@ func TorrentSearchPage(w http.ResponseWriter, r *http.Request) {
 	pageStruct.SiteName = webdata.SiteName
 	pageStruct.PageName = "Search Results"
 
-	webutils.RenderTemplate(w, []string{"pkg/web/templates/sidebar.html", "pkg/web/templates/search.html",
+	webutils.RenderTemplate(c.Writer, []string{
+		"pkg/web/templates/sidebar.html",
+		"pkg/web/templates/search.html",
 		"pkg/web/templates/torrent-list-item.html",
 		"pkg/web/templates/head.html",
 		"pkg/web/templates/end.html",
-		"pkg/web/templates/commandbar.html"}, pageStruct)
+		"pkg/web/templates/commandbar.html",
+	}, pageStruct)
 }
